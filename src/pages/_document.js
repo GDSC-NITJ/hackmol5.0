04ece80cf9ce1a95ffe958c44933035389a998da
Alok Kumar <122669937+alokthakur490@users.noreply.github.com>
2024-01-10 21:07:54 +0530

    Revert "prize_section"

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -40,7 +40,6 @@ export default function Document() {
                     <meta name="twitter:domain" content="https://hackmol4.tech" />
                     <meta name="twitter:app:name:iphone" content="HackMOL 4.0" />
                     <meta name="twitter:app:name:ipad" content="HackMOL 4.0" />
-                    
 
                 </Head>
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,11 @@
 // import Schedule from '../components/Timeline/Schedule'
 // import Script from 'next/script'
 
-import PrizeSection from '../components/PrizeSection'
-
 export default function Home() {
 
   return (
     <div className=''>
-
       hi
-          <PrizeSection/>
     </div>
   )
 }
