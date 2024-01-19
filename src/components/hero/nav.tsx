29b052aff86d9c398d5c4b7bba6514ff2d0e153e
Shubhi Gulati <shubhigulati2534@gmail.com>
2024-01-19 11:49:59 +0530

    changes

--- src/components/hero/nav.tsx
+++ src/components/hero/nav.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import React from 'react';
-import {useRouter} from "next/router"
+
 
 export default function NavBar  ()  {
 
